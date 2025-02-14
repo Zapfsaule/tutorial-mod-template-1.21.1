3fa9d59addb3f825989f12cc6ef4260241f230a8
Zapfsaule <XKfel.sbk@outlook.com>
2025-02-14 19:41:27 +0100

    Update

--- src/main/java/net/zapfsaule/tutorialmod/TutorialMod.java
+++ src/main/java/net/zapfsaule/tutorialmod/TutorialMod.java
@@ -2,15 +2,22 @@ package net.zapfsaule.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.zapfsaule.tutorialmod.block.ModBlocks;
+import net.zapfsaule.tutorialmod.item.ModItemGroups;
+import net.zapfsaule.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {;
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+
 	}
 }
